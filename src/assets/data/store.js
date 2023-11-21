9e3a7699254389f29276be3713b840f657f787a5
matteobonelli <matteobonelli08@gmail.com>
2023-11-21 13:13:37 +0100

    header completed

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -1,5 +1,11 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-
+    telephoneNum: '(001)88451234 88455438',
+    socials: [
+        'fa-facebook-f', 'fa-twitter', 'fa-pinterest-p'
+    ],
+    navbar: [
+        'Home', 'About', 'Services', 'Showcase', 'Blog', 'Contact'
+    ]
 })
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,82 @@
+<template>
+    <header>
+        <div class="infos-bg">
+            <div class="container d-flex justify-content-between align-items-center">
+                <div>{{ store.telephoneNum }}</div>
+                <ul>
+                    <li v-for="social in store.socials" class="me-3">
+                        <i class='fa-brands ' :class="social"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav class="d-flex justify-content-between align-items-center ">
+            <img src="../assets/images/log.png" alt="logo">
+            <ul>
+                <li v-for="tab in store.navbar" class="me-4 text-uppercase ">
+                    <a href="#">{{ tab }}</a>
+                </li>
+            </ul>
+            <div>
+                <button class="btn me-5 text-uppercase">Purchase</button>
+                <i class="fa-solid fa-magnifying-glass me-3"></i>
+            </div>
+        </nav>
+
+    </header>
+</template>
+
+<script>
+import { store } from '../assets/data/store'
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+header {
+    background-color: $brand_primary;
+}
+
+nav {
+    padding: 40px;
+}
+
+ul {
+    display: flex;
+}
+
+a {
+    text-decoration: none;
+    color: $text_navbar;
+}
+
+.fa-magnifying-glass {
+    color: $magnifying_lens;
+    font-size: 1vw
+}
+
+.infos-bg {
+    background-color: $brand_infos;
+    color: white;
+    padding: 10px 0;
+}
+
+.btn {
+    background-color: $magnifying_lens;
+    color: white;
+    padding: 20px 60px;
+    border-radius: 40px;
+}
+
+.container {
+    max-width: 1700px;
+}
+</style>
